4960dcef8593dd68e1fadbf876215cedb247a42e
mia2583 <mia2583@pusan.ac.kr>
2025-04-29 23:01:31 +0900

    style: define ocr model to config file

--- libro_server/libro_ai_server/src/book_recognition/book_recognition/config/config.py
+++ libro_server/libro_ai_server/src/book_recognition/book_recognition/config/config.py
@@ -5,6 +5,8 @@ from ament_index_python.packages import get_package_share_directory
 import os
 from ultralytics import YOLO
 import torch
+from paddleocr import PaddleOCR
+
 
 pkg_path = get_package_share_directory('book_recognition')
 
@@ -14,3 +16,5 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 yolo_model = YOLO(os.path.join(pkg_path, 'models', 'yolo11s_best.pt')).to(device)
 # yolo_model = YOLO(os.path.join(pkg_path, 'models', 'yolo11n-obb-only-book.pt'))
 # yolo_model = YOLO(os.path.join(pkg_path, 'models', 'yolo11n-obb-book-and-label.pt'))
+
+ocr_model = PaddleOCR(lang="korean", use_gpu=False, use_angle_cls=True, show_log=False)
