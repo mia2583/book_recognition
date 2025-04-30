0d40caece358cfe9f25d3dc7b070fd6e749fce08
mia2583 <mia2583@pusan.ac.kr>
2025-04-30 16:06:23 +0900

    feat: Add tcp connection for book recognition

---
+++ book_tcp/tcp_client.py
@@ -0,0 +1,28 @@
+## CLIENT ##
+
+import socket
+from _thread import *
+
+HOST = 'server_ip' # 서버의 ip로 적어줄 것. `ip a | grep inet`를 통해서 확인할 수 있다. 
+PORT = 9999
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
+
+def recv_data(client_socket):
+    while True:
+        data = client_socket.recv(1024)
+        print("recive : ", repr(data.decode()))
+
+start_new_thread(recv_data, (client_socket,))
+print('>> Connect Server')
+
+while True:
+    message = input()
+    if message == 'quit':
+        close_data = message
+        break
+
+    client_socket.send(message.encode())
+
+client_socket.close()
